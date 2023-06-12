137b74bc00d2e2f455593ef3026fa540279b1af2
Robot7769 <jan.robot7@gmail.com>
2023-06-12 02:06:18 +0200

    feat: add Rectangle

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+class Rectangle {
+private:
+    int width;
+    int height;
+
+public:
+    Rectangle(int w, int h) {
+        width = w;
+        height = h;
+    }
+
+    int getArea() {
+        return width * height;
+    }
+};
+
+int main() {
+    Rectangle rectangle(5, 3);
+    cout << "Plocha obdélníku: " << rectangle.getArea() << endl;
+
+    return 0;
+}
